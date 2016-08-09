5e72fcf9bd55e8a3f6fdc17cf76c5f7d080d8e11
Tdpiamix <Tdpiamix@users.noreply.github.com>
2016-08-09 12:17:18 +0800

    Update handlers.py

--- www/handlers.py
+++ www/handlers.py
@@ -1,18 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-'URL处理函数'
+'''URL处理函数'''
 
-import re, time, json, logging, hashlib, base64, asyncio
-
-from coroweb import get, post
-
-from models import User, Comment, Blog, next_id
-
-@get('/')
-async def index(request):
-    users = await User.findAll()
-    return {
-        '__template__': 'test.html',
-        'users': users
-    }
+' url handlers '
