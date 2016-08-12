3538bc95447bcd2c204149af8bf179eceef94e97
Tdpiamix <Tdpiamix@users.noreply.github.com>
2016-08-12 23:47:14 +0800

    Update app.py

--- www/app.py
+++ www/app.py
@@ -13,9 +13,13 @@ from aiohttp import web
 
 from jinja2 import Environment, FileSystemLoader
 
+from config import configs
+
 import orm
 
 from coroweb import add_routes, add_static
+
+from handlers import cookie2user, COOKIE_NAME
 
 #初始化jinja2模板
 def init_jinja2(app, **kw):
@@ -61,6 +65,22 @@ async def logger_factory(app, handler):
         logging.info('Request: %s %s' % (request.method, request.path))
         return (await handler(request))
     return logger
+
+#
+async def auth_factory(app, handler):
+    async def auth(request):
+        logging.info('check user: %s %s' % (request.method, request.path))
+        request.__user__ = None
+        cookie_str = request.cookies.get(COOKIE_NAME)
+        if cookie_str:
+            user = await cookie2user(cookie_str)
+            if user:
+                logging.info('set current user: %s' % user.email)
+                request.__user__ = user
+        if request.path.startswith('/manage/') and (request.__user__ is None or not request.__user__.admin):
+            return web.HTTPFound('/signin')
+        return (await handler(request))
+    return auth
 
 #在处理URL请求前，将消息主体内容记录下来
 async def data_factory(app, handler):
@@ -109,6 +129,7 @@ async def response_factory(app, handler):
                 return resp
             #否则，
             else:
+                r['__user__'] = request.__user__
                 resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
                 resp.content_type = 'text/html;charset=utf-8'
                 return resp
@@ -153,7 +174,7 @@ async def init(loop):
     await orm.create_pool(loop=loop, user='www-data', password='www-data', db='awesome')
     #创建web应用，循环类型为消息循环
     app = web.Application(loop=loop, middlewares=[
-        logger_factory, response_factory
+        logger_factory, auth_factory, response_factory
     ])  
     init_jinja2(app, filters=dict(datetime=datetime_filter))
     #注册URL处理函数
